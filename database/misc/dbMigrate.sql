4121d8352f53ade92f0d15978d87c696e2d7951e
Ernad Husremovic <hernad@bring.out.ba>
2011-10-14 14:03:36 +0200

    dbMigrate example

---
+++ database/misc/dbMigrate.sql
@@ -0,0 +1,100 @@
+--
+-- This file is part of the knowhow ERP, a free and open source
+-- Enterprise Resource Planning software suite,
+-- Copyright (c) 2010-2011 by bring.out doo Sarajevo.
+-- It is licensed to you under the Common Public Attribution License
+-- version 1.0, the full text of which (including knowhow-specific Exhibits)
+-- is available in the file LICENSE_CPAL_bring.out_knowhow.md located at the
+-- root directory of this source code archive.
+-- By using this software, you agree to be bound by its terms.
+--
+
+------------------------------------------------
+-- init
+-- create table t1
+------------------------------------------------
+SELECT EXECUTE($$
+
+CREATE TABLE IF NOT EXISTS skeleton.t1
+(
+    p1 character(2),
+    d1 date,
+    n1 numeric(17,2)
+);
+
+
+$$)
+WHERE  u2.package_version('skeleton') <= 0;
+------------------------------------------------
+-- end 
+-------------------------------------------------
+
+
+
+------------------------------------------------
+-- 0.1.0, 0.2.0
+-- create table t2
+------------------------------------------------
+SELECT EXECUTE($$
+
+CREATE TABLE IF NOT EXISTS skeleton.t2
+(
+    p21 character(2),
+    d21 date,
+    n21 numeric(17,2)
+);
+
+
+$$)
+WHERE NOT u2.package_version('skeleton') <= 200;
+------------------------------------------------
+-- end 
+-------------------------------------------------
+
+------------------------------------------------
+-- 0.3.0 - 0.3.99
+-- create table t3
+------------------------------------------------
+SELECT EXECUTE($$
+
+CREATE TABLE IF NOT EXISTS skeleton.t3
+(
+    p31 character(2),
+    d31 date,
+    n31 numeric(17,2)
+);
+
+
+$$)
+WHERE NOT u2.package_version('skeleton') <= 399;
+
+
+------------------------------------------------
+-- 0.4.0
+-- drop table t1
+------------------------------------------------
+SELECT EXECUTE($$
+
+DROP TABLE t1;
+
+$$)
+WHERE NOT u2.package_version('skeleton') <= 400;
+
+
+------------------------------------------------
+-- 0.5.x
+-- create table t1 v2
+------------------------------------------------
+SELECT EXECUTE($$
+
+CREATE TABLE IF NOT EXISTS skeleton.t1
+(
+    p1v2 character(10),
+    d1v2 date,
+    n1v2 numeric(25,2)
+);
+
+
+
+$$)
+WHERE NOT u2.package_version('skeleton') <= 599;
---
+++ database/misc/initPackage.sql
@@ -0,0 +1,15 @@
+--
+-- This file is part of the knowhow ERP, a free and open source
+-- Enterprise Resource Planning software suite,
+-- Copyright (c) 2010-2011 by bring.out doo Sarajevo.
+-- It is licensed to you under the Common Public Attribution License
+-- version 1.0, the full text of which (including knowhow-specific Exhibits)
+-- is available in the file LICENSE_CPAL_bring.out_knowhow.md located at the
+-- root directory of this source code archive.
+-- By using this software, you agree to be bound by its terms.
+--
+
+COMMENT ON SCHEMA skeleton IS 'knowhow/xtuple package skeleton';
+
+GRANT ALL ON SCHEMA skeleton TO xtrole;
+
