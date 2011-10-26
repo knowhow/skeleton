303c84ffa32e04ef581442e90bba3340f3d0ff46
Ernad Husremovic <hernad@bring.out.ba>
2011-10-26 17:20:20 +0200

    make_pkg.sh

--- make_pkg.sh
+++ make_pkg.sh
@@ -13,6 +13,9 @@ fi
 
 
 mkdir package
-rm package/$PK_NAME.gz
-$TAR cvfz package/$PKG_NAME.gz ../$PKG_DIR --exclude=.gitignore --exclude=package --exclude=*.gz --exclude=.git
+rm package/$PKG_NAME.gz
 
+cd ..; $TAR cvfz $PKG_DIR/package/$PKG_NAME.gz  --exclude="*/.gitignore" --exclude="*/package" --exclude="*.gz" --exclude="*/.git" $PKG_DIR
+
+cd $PKG_DIR
+ls package/$PKG_NAME.gz
