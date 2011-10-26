269c6aa5ffbd9fe0df1e02db6375f911b9cd6bf9
Ernad Husremovic <hernad@bring.out.ba>
2011-10-26 17:13:19 +0200

    tar/gnutar

--- make_pkg.sh
+++ make_pkg.sh
@@ -3,7 +3,16 @@
 PKG_NAME=skeleton
 PKG_DIR=skeleton
 
+OS_MAC_OSX = `uname -a | grep -c Darwin`
+
+if [ OS_MAC_OSX == "1" ]; then
+    TAR=gnutar
+else
+    TAR=tar
+fi
+
+
 mkdir package
 rm package/$PK_NAME.gz
-gnutar cvfz package/$PKG_NAME.gz ../$PKG_DIR --exclude=.gitignore --exclude=package --exclude=*.gz --exclude=.git
+$TAR cvfz package/$PKG_NAME.gz ../$PKG_DIR --exclude=.gitignore --exclude=package --exclude=*.gz --exclude=.git
 
